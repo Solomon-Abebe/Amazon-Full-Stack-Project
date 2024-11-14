b2379fda54d38a151a4908ba2fbb8832885c1bf8
Solomon-Abebe <solomonabebe23@gmail.com>
2024-11-14 18:01:32 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        Amazon Project
+        </div>
+       
+    </>
+  )
+}
+
+export default App
