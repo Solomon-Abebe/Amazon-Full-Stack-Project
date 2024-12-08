e4c27bc5365fb78b49aed7fb2b0fe6b3b217aaf3
Solomon-Abebe <solomonabebe23@gmail.com>
2024-12-08 03:17:49 +0300

    API key secured in env

--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -4,14 +4,26 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDuPl25k9hHtMzl-oeHAaUZhIbkkoYPXp4",
+//   authDomain: "project-63251.firebaseapp.com",
+//   projectId: "project-63251",
+//   storageBucket: "project-63251.appspot.com",
+//   messagingSenderId: "662372623732",
+//   appId: "1:662372623732:web:f21a14331e930657d9f821",
+// };
+
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDuPl25k9hHtMzl-oeHAaUZhIbkkoYPXp4",
-  authDomain: "project-63251.firebaseapp.com",
-  projectId: "project-63251",
-  storageBucket: "project-63251.appspot.com",
-  messagingSenderId: "662372623732",
-  appId: "1:662372623732:web:f21a14331e930657d9f821",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
